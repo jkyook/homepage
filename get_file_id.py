8891bcb5d81e5fda9369d26bedd4da8a3038ebe3
jinkwan.yook@gmail.com <jinkwani79>
2024-08-06 10:28:19 +0900

    file_id, token

---
+++ get_file_id.py
@@ -0,0 +1,37 @@
+from googleapiclient.discovery import build
+import pickle
+import os.path
+
+
+SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
+
+def main():
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('drive', 'v3', credentials=creds)
+
+    # Google Drive에서 파일 리스트를 가져옵니다.
+    results = service.files().list(pageSize=50, fields="nextPageToken, files(id, name)").execute()
+    items = results.get('files', [])
+
+    if not items:
+        print('No files found.')
+    else:
+        print('Files:')
+        for item in items:
+            print(f"{item['name']} ({item['id']})")
+
+if __name__ == '__main__':
+    main()
